960e30fc7ca5a058dc79efc11abb0cb113fea26f
zakstucke <44890343+zakstucke@users.noreply.github.com>
2025-03-22 17:47:30 +0000

    codec (#10)

--- leptos-fetch/src/resource_drop_guard.rs
+++ leptos-fetch/src/resource_drop_guard.rs
@@ -1,56 +1,52 @@
-use std::{any::TypeId, hash::Hash, sync::Arc};
+use std::{any::TypeId, sync::Arc};
 
 use parking_lot::Mutex;
 
-use crate::cache::ScopeLookup;
+use crate::{cache::ScopeLookup, utils::KeyHash};
 
 #[derive(Clone)]
-pub struct ResourceDropGuard<K, V>(Arc<Mutex<ResourceDropGuardInner<K, V>>>)
+pub struct ResourceDropGuard<V>(Arc<Mutex<ResourceDropGuardInner<V>>>)
 where
-    K: Eq + Hash + 'static,
     V: 'static;
 
-impl<K, V> ResourceDropGuard<K, V>
+impl<V> ResourceDropGuard<V>
 where
-    K: Eq + Hash + 'static,
     V: 'static,
 {
     pub fn new(scope_lookup: ScopeLookup, resource_id: u64, cache_key: TypeId) -> Self {
-        ResourceDropGuard(Arc::new(Mutex::new(ResourceDropGuardInner::<K, V> {
+        ResourceDropGuard(Arc::new(Mutex::new(ResourceDropGuardInner::<V> {
             scope_lookup,
             resource_id,
-            active_key: None,
+            active_key_hash: None,
             cache_key,
             _phantom: std::marker::PhantomData,
         })))
     }
 
-    pub fn set_active_key(&self, active_key: K) {
-        self.0.lock().active_key = Some(active_key);
+    pub fn set_active_key(&self, active_key_hash: KeyHash) {
+        self.0.lock().active_key_hash = Some(active_key_hash);
     }
 }
 
-struct ResourceDropGuardInner<K, V>
+struct ResourceDropGuardInner<V>
 where
-    K: Eq + Hash + 'static,
     V: 'static,
 {
     scope_lookup: ScopeLookup,
     resource_id: u64,
-    active_key: Option<K>,
+    active_key_hash: Option<KeyHash>,
     cache_key: TypeId,
     _phantom: std::marker::PhantomData<V>,
 }
 
-impl<K, V> Drop for ResourceDropGuardInner<K, V>
+impl<V> Drop for ResourceDropGuardInner<V>
 where
-    K: Eq + Hash + 'static,
     V: 'static,
 {
     fn drop(&mut self) {
-        if let Some(active_key) = self.active_key.take() {
-            self.scope_lookup.mark_resource_dropped::<K, V>(
-                active_key,
+        if let Some(active_key_hash) = self.active_key_hash.take() {
+            self.scope_lookup.mark_resource_dropped::<V>(
+                &active_key_hash,
                 &self.cache_key,
                 self.resource_id,
             );
